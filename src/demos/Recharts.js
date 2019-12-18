c686fff8bd324622cabe1bdb9a0959dc42147f82
Nick Banks <nbanks@awolff.com>
2019-12-18 16:59:39 -0700

    Finished Recharts example custom tooltop

--- src/demos/Recharts.js
+++ src/demos/Recharts.js
@@ -6,27 +6,40 @@ import {
 
   const data = [
     {
-      name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+      name: 'Page A', value: 78,
     },
     {
-      name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+      name: 'Page B', value: 64,
     },
     {
-      name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+      name: 'Page C', value: 35,
     },
     {
-      name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+      name: 'Page D', value: 15,
     },
     {
-      name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+      name: 'Page E', value: 26,
     },
     {
-      name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+      name: 'Page F', value: 38,
     },
     {
-      name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+      name: 'Page G', value: 97,
     },
   ];
+
+  const CustomTooltip = ({ active, payload, label }) => {
+    if (active) {
+      return (
+        <div style={{backgroundColor: 'white', opacity: '.8', padding: '10px'}}>
+          <p className="label">{`${label} : ${payload[0].value}`}</p>
+          <p>My amazing custom text</p>
+        </div>
+      );
+    }
+  
+    return null;
+  };
 
 class Recharts extends Component {
     render() {
@@ -44,10 +57,9 @@ class Recharts extends Component {
                         <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="name" />
                         <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Bar dataKey="pv" fill="#8884d8" />
-                        <Bar dataKey="uv" fill="#82ca9d" />
+                        <Tooltip content={<CustomTooltip />}/>
+                        {/* <Legend /> */}
+                        <Bar dataKey="value" fill="#8884d8" />
                 </BarChart>
             </div>
         );
